9d6daf595e2a7393f7d0b4a62f3ee6fa42b88547
younger.shen <younger.x.shen@gmail.com>
2013-03-18 16:19:12 +0800

    push test

---
+++ src/com/youngershen/proj/lucia/junit/MainTest.java
@@ -0,0 +1,18 @@
+/**
+ * 2013-3-18
+ * 下午4:18:59
+ * lucia
+ * com.youngershen.proj.lucia.junit
+ * author : younger.shen
+ * email  : younger.x.shen@gmail.com
+ * 
+ */
+package com.youngershen.proj.lucia.junit;
+
+/**
+ * @author youngershen
+ *
+ */
+public class MainTest {
+
+}
